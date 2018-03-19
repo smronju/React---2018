541d849e4a8008db777dbb9791289ab135e136ec
Mohammad Shoriful Islam Ronju <smronju@gmail.com>
2018-03-19 22:32:02 +0600

    App and other component added

---
+++ src/components/Inventory.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Inventory extends Component {
+  render() {
+    return <div className="inventory">Inventory Component</div>;
+  }
+}
+
+export default Inventory;
