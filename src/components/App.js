5056499919df8003e134250702cbc2bd802b762d
Mohammad Shoriful Islam Ronju <smronju@gmail.com>
2018-03-27 23:21:28 +0600

    Understanding state and props

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,20 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends Component {
+  state = {
+    fishes: {},
+    order: {},
+  };
+
+  addFish = fish => {
+    // 1. Take a copy of existing state
+    const fishes = { ...this.state.fishes };
+    // 2. Add our new fiesh to fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // Set the new fishesh object to state
+    this.setState({ fishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -12,7 +26,7 @@ class App extends Component {
         </div>
 
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends Component {
   render() {
-    return <div className="inventory">Inventory Component</div>;
+    return (
+      <div className="inventory">
+        <h2>Inventory</h2>
+
+        <AddFishForm addFish={this.props.addFish} />
+      </div>
+    );
   }
 }
 
