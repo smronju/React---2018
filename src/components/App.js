41a851cd84cb0694aae1bbfda13b2ae824909464
Mohammad Shoriful Islam Ronju <smronju@gmail.com>
2018-03-29 00:00:55 +0600

    Loading data into state

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends Component {
   state = {
@@ -18,6 +19,10 @@ class App extends Component {
     this.setState({ fishes });
   };
 
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -26,7 +31,7 @@ class App extends Component {
         </div>
 
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     );
   }
