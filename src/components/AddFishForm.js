a071495ce5448b9b49f22a6ac41a76d26075f7b5
Mohammad Shoriful Islam Ronju <smronju@gmail.com>
2018-05-06 02:03:21 +0600

    AddFishForm lint correction

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class AddFishForm extends Component {
   nameRef = React.createRef();
@@ -40,5 +41,9 @@ class AddFishForm extends Component {
     );
   }
 }
+
+AddFishForm.propTypes = {
+  addFish: PropTypes.func.isRequired,
+};
 
 export default AddFishForm;
